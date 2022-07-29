732f1577ba281e44e0784cfc87d37077a7c72830
FerSosa21 <54343796+FeerSosa@users.noreply.github.com>
2022-07-28 23:48:18 -0300

    ejercicio tema 1

---
+++ ejJavaBasicoOpenBootcamp/src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+// Entrega Tema 1
+public class Main {
+    public static void main(String[] args) {
+
+        //NUMEROS
+        short num1 = 3;
+        int num2 = 20;
+        long num3 = 1000;
+        float num4 = 1.3f;
+        double num5 = 2.533d;
+
+        //LOGICO
+        boolean falso = false;
+        boolean verdadero = true;
+
+        //Texto
+        char caracter = 'a';
+        String texto = "Hola";
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(falso);
+        System.out.println(verdadero);
+        System.out.println(caracter);
+        System.out.println(texto);
+
+    }
+}
